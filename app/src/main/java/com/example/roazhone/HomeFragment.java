c2a029f8c05c30453a67f113f548fefd710f2f33
Equilat <emile.georget@outlook.fr>
2021-02-24 15:31:11 +0100

    fix main no more tooltip on long click on menu item

--- app/src/main/java/com/example/roazhone/HomeFragment.java
+++ app/src/main/java/com/example/roazhone/HomeFragment.java
@@ -2,18 +2,41 @@ package com.example.roazhone;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.widget.TooltipCompat;
 import androidx.fragment.app.Fragment;
 
-public class HomeFragment extends Fragment {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeFragment extends Fragment implements View.OnLongClickListener {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View myView = inflater.inflate(R.layout.home_fragment, container, false);
+        bottomNavigationView = myView.findViewById(R.id.activity_main_bottom_navigation);
+        disableMenuTooltip();
+
         return myView;
+    }
+
+    private void disableMenuTooltip(){
+        View parkingLogo = bottomNavigationView.getChildAt(0).findViewById(R.id.parkingLogo);
+        View parkAndRideLogo = bottomNavigationView.getChildAt(0).findViewById(R.id.parkAndRideLogo);
+
+        parkingLogo.setOnLongClickListener(this);
+        parkAndRideLogo.setOnLongClickListener(this);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+
+        return true;
     }
 }
