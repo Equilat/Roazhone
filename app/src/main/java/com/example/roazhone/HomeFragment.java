3c21fd4a2df24613012d8b4335825458dfd60bd1
Equilat <emile.georget@outlook.fr>
2021-02-25 16:45:37 +0100

    improve main reduced card size

--- app/src/main/java/com/example/roazhone/HomeFragment.java
+++ app/src/main/java/com/example/roazhone/HomeFragment.java
@@ -11,6 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -78,6 +79,8 @@ public class HomeFragment extends Fragment implements View.OnLongClickListener, 
         parkingLogo.setOnLongClickListener(this);
         parkAndRideLogo.setOnLongClickListener(this);
     }
+
+
 
 
 
--- app/src/main/java/com/example/roazhone/UndergroundParkingViewHolder.java
+++ app/src/main/java/com/example/roazhone/UndergroundParkingViewHolder.java
@@ -3,18 +3,27 @@ package com.example.roazhone;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 
-public class UndergroundParkingViewHolder extends RecyclerView.ViewHolder {
+public class UndergroundParkingViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
     protected TextView vName;
     protected TextView vRoom;
 
     public UndergroundParkingViewHolder(View v) {
         super(v);
+        v.setOnClickListener(this);
         vName =  v.findViewById(R.id.parkingName);
         vRoom = v.findViewById(R.id.parkingRoom);
+    }
+
+    @Override
+    public void onClick(View v) {
+        System.out.println("here");
+        int itemPosition = this.getAdapterPosition();
+        Navigation.findNavController(v).navigate(R.id.actionHomeFragmentToInfosParkingFragment);
     }
 
 
