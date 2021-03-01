864b5202678a4cf66eee5d4e588babd6f3214053
Equilat <emile.georget@outlook.fr>
2021-03-01 01:18:03 +0100

    fix main c-park underground parkings opening status is not working (status remains to open, even if parking is closed). A temporary solution is to include "hard values" with opening hours of these parkings. These are stored in a XML file called underground_parking_opening_hours.xml. A check of the API value is maintained. In the case it would be fixed by C-Park, dynamic value would be used again in parallel of the XML. A computation is made of the XML values to calculate the opening status of the parking.

--- app/src/main/java/com/example/roazhone/UndergroundParkingAdapter.java
+++ app/src/main/java/com/example/roazhone/UndergroundParkingAdapter.java
@@ -14,15 +14,26 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.roazhone.model.UndergroundParkingDetails;
 
+import java.lang.reflect.Array;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
+import java.util.TimeZone;
 
 public class UndergroundParkingAdapter extends RecyclerView.Adapter<UndergroundParkingViewHolder> {
 
     private List<UndergroundParkingDetails> parkingList;
     private Context context;
     private Set<String> parkingsFavoris;
+    private View itemView;
+
 
     public UndergroundParkingAdapter(Context context) {
         this.context = context;
@@ -43,7 +54,7 @@ public class UndergroundParkingAdapter extends RecyclerView.Adapter<UndergroundP
     @NonNull
     @Override
     public UndergroundParkingViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.
+        this.itemView = LayoutInflater.
                 from(parent.getContext()).
                 inflate(R.layout.card_layout, parent, false);
 
@@ -63,21 +74,25 @@ public class UndergroundParkingAdapter extends RecyclerView.Adapter<UndergroundP
             vh.vFavoris.setVisibility(View.INVISIBLE);
         }
 
-        if(upd.getStatus().equals("FERME")) {
-            vh.vRoom.setText(R.string.parking_ferme_short);
-            vh.vRoom.setTextColor(ContextCompat.getColor(this.context, R.color.roazhone_red));
-        }
-        else if(upd.getPlacesLibres() == 0) {
-            vh.vRoom.setText(R.string.parking_complet_short);
-            vh.vRoom.setTextColor(ContextCompat.getColor(this.context, R.color.roazhone_red));
-        }
-        else if(upd.getPlacesLibres() <= upd.getPlacesMax()*0.2) {
-            vh.vRoom.setText(upd.getPlacesLibres().toString()+context.getString(R.string.places_dispos));
-            vh.vRoom.setTextColor(ContextCompat.getColor(this.context, R.color.roazhone_orange));
-        }
-        else {
-            vh.vRoom.setText(upd.getPlacesLibres().toString()+context.getString(R.string.places_dispos));
-            vh.vRoom.setTextColor(ContextCompat.getColor(this.context, R.color.roazhone_green));
+        try {
+            if(!isOpened(upd.getNomParking()) || upd.getStatus().equals("FERME")) {
+                vh.vRoom.setText(R.string.parking_ferme_short);
+                vh.vRoom.setTextColor(ContextCompat.getColor(this.context, R.color.roazhone_red));
+            }
+            else if(upd.getPlacesLibres() == 0) {
+                vh.vRoom.setText(R.string.parking_complet_short);
+                vh.vRoom.setTextColor(ContextCompat.getColor(this.context, R.color.roazhone_red));
+            }
+            else if(upd.getPlacesLibres() <= upd.getPlacesMax()*0.2) {
+                vh.vRoom.setText(upd.getPlacesLibres().toString()+context.getString(R.string.places_dispos));
+                vh.vRoom.setTextColor(ContextCompat.getColor(this.context, R.color.roazhone_orange));
+            }
+            else {
+                vh.vRoom.setText(upd.getPlacesLibres().toString()+context.getString(R.string.places_dispos));
+                vh.vRoom.setTextColor(ContextCompat.getColor(this.context, R.color.roazhone_green));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
 
         List<Double> coord = upd.getGeo();
@@ -99,6 +114,113 @@ public class UndergroundParkingAdapter extends RecyclerView.Adapter<UndergroundP
     @Override
     public int getItemCount() {
         return parkingList.size();
+    }
+
+    private boolean isOpened(String parkingName) throws ParseException {
+        boolean ret = false;
+        Calendar calendarToday = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
+        Calendar calendarTomorrow = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
+        calendarTomorrow.add(Calendar.DATE, 1);
+        int dayOfWeek = calendarToday.get(Calendar.DAY_OF_WEEK);
+        String[] parkingWeeklyOpeningHours = getParkingOpeningHours(parkingName);
+        String[] parkingWeeklyClosingHours = getParkingClosingHours(parkingName);
+        String parkingOpeningHours = parkingWeeklyOpeningHours[dayOfWeek - 1];
+        String parkingClosingHours = parkingWeeklyClosingHours[dayOfWeek - 1];
+        if(!parkingOpeningHours.equals("fermé")) {
+            Date parkingOpeningTime = new SimpleDateFormat("HH:mm", Locale.FRANCE).parse(parkingOpeningHours);
+            Date parkingClosingTime = new SimpleDateFormat("HH:mm", Locale.FRANCE).parse(parkingClosingHours);
+            Date currentTime = calendarToday.getTime();
+            if(parkingOpeningHours.equals(parkingClosingHours)) {
+                ret = true;
+            }
+            else if(parkingClosingHours.compareTo("00:00") >= 0) {
+                calendarTomorrow.setTime(parkingClosingTime);
+                if((currentTime.after(parkingOpeningTime) ||currentTime.equals(parkingOpeningTime)) && currentTime.before(calendarTomorrow.getTime())) {
+                    ret = true;
+                }
+            }
+            else {
+                calendarToday.setTime(parkingClosingTime);
+                if((currentTime.after(parkingOpeningTime) ||currentTime.equals(parkingOpeningTime)) && currentTime.before(calendarToday.getTime())) {
+                    ret = true;
+                }
+            }
+        }
+        return ret;
+    }
+
+    private String[] getParkingOpeningHours(String parkingName) {
+        String days = "";
+        switch (parkingName) {
+            case "Colombier":
+                days = itemView.getResources().getString(R.string.colombier_ouverture);
+                break;
+            case "Gare-Sud":
+                days = itemView.getResources().getString(R.string.gare_sud_ouverture);
+                break;
+            case "Dinan-Chezy":
+                days = itemView.getResources().getString(R.string.dinan_chezy_ouverture);
+                break;
+            case "Vilaine":
+                days = itemView.getResources().getString(R.string.vilaine_ouverture);
+                break;
+            case "Hoche":
+                days = itemView.getResources().getString(R.string.hoche_ouverture);
+                break;
+            case "Kennedy":
+                days = itemView.getResources().getString(R.string.kennedy_ouverture);
+                break;
+            case "Lices":
+                days = itemView.getResources().getString(R.string.lices_ouverture);
+                break;
+            case "Charles-de-gaulle":
+                days = itemView.getResources().getString(R.string.charles_de_gaulle_ouverture);
+                break;
+            case "Kleber":
+                days = itemView.getResources().getString(R.string.kleber_ouverture);
+                break;
+            case "Arsenal":
+                days = itemView.getResources().getString(R.string.arsenal_ouverture);
+                break;
+        }
+        return days.split(",");
+    }
+
+    private String[] getParkingClosingHours(String parkingName) {
+        String days = "";
+        switch (parkingName) {
+            case "Colombier":
+                days = itemView.getResources().getString(R.string.colombier_fermeture);
+                break;
+            case "Gare-Sud":
+                days = itemView.getResources().getString(R.string.gare_sud_fermeture);
+                break;
+            case "Dinan-Chezy":
+                days = itemView.getResources().getString(R.string.dinan_chezy_fermeture);
+                break;
+            case "Vilaine":
+                days = itemView.getResources().getString(R.string.vilaine_fermeture);
+                break;
+            case "Hoche":
+                days = itemView.getResources().getString(R.string.hoche_fermeture);
+                break;
+            case "Kennedy":
+                days = itemView.getResources().getString(R.string.kennedy_fermeture);
+                break;
+            case "Lices":
+                days = itemView.getResources().getString(R.string.lices_fermeture);
+                break;
+            case "Charles-de-gaulle":
+                days = itemView.getResources().getString(R.string.charles_de_gaulle_fermeture);
+                break;
+            case "Kleber":
+                days = itemView.getResources().getString(R.string.kleber_fermeture);
+                break;
+            case "Arsenal":
+                days = itemView.getResources().getString(R.string.arsenal_fermeture);
+                break;
+        }
+        return days.split(",");
     }
 
     public void setParkings(List<UndergroundParkingDetails> parkingList) {
